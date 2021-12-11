b8ac52eff0a1540e39b2ea77db22929664f0801b
Patrick Babala <babalapatrick@gmail.com>
2021-12-11 10:07:51 +0800

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>ExoTech</title>
+        <title>ExoTechnginamo</title>
 
         <link href="css/main.css?v=<?php echo time(); ?>" rel="stylesheet">
         
