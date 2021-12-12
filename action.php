abf4c4e6eb1176dd0f75d7694beb12e89e9d3b20
KenAlupit <alupitkenadrian22@gmail.com>
2021-12-12 13:20:56 +0800

    database change

--- action.php
+++ action.php
@@ -1,6 +1,6 @@
 <?php 
 session_start();
-require_once "dbconfic.php";
+require_once "dbconfig.php";
 
 if (isset($_POST['Product_ID']) && isset($_POST['Name']) && isset($_POST['Category']) && isset($_POST['Price'])){
     $id = $_POST["Product_ID"];
